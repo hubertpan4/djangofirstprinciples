e5c4dd1544d95e66eb1f8c1006bd9c79cfbf25dc
Hubert Pan <hubert.pan4@gmail.com>
2024-05-01 22:40:54 -0400

    initial commit

---
+++ blogmaker_lite.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.http import HttpResponse
+from django.urls import path
+from django.core.handlers.wsgi import WSGIHandler
+from django.core.management import execute_from_command_line
+
+settings.configure(
+    ROOT_URLCONF = __name__, # tells django where to find the urls it should listen to, in this case it is this file
+    DEBUG=True, # tells django to show debugging info if something goes wrong 
+    SECRET_KEY="my-secret-key"
+)
+
+def index(request):
+    return HttpResponse("BlogMaker Lite")
+
+def bob(request):
+    return HttpResponse("Hi Bob!")
+
+urlpatterns = [
+    path("", index),
+    path("bob", bob)
+]
+
+application = WSGIHandler()
+
+if __name__ == "__main__":
+    execute_from_command_line()
+    # run this file with the command `python blogmaker_lite.py runserver`
